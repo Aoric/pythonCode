63266af36a739c300b493df4ce57409aba711a02
Aoric <aoric268@163.com>
2019-11-25 20:34:32 +0800

    Revert "Revert "01""
    This reverts commit fd3b9ed3883e29bcd0aeef4cf14d801a3fda3b43.

---
+++ cource2.py
@@ -0,0 +1,17 @@
+"""
+Author:Adrian
+"""
+# import this
+import turtle
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
---
+++ hello.py
@@ -0,0 +1,2 @@
+name = input('请命名：')
+print("hello,I'm",name)
